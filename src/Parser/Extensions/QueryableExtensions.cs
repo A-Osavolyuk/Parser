eb2af2959028db644ad24d8fafd2a4d8184202cb
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-13 19:39:20 +0100

    Add support for tokens filtering and ordering

---
+++ src/Parser/Extensions/QueryableExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Parser.Data.Entities;
+using Parser.Filtering;
+
+namespace Parser.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> ApplyOrdering<T>(this IQueryable<T> query, string propertyName)
+    {
+        var descending = propertyName.Contains('-');
+        var purePropertyName = new StringBuilder(propertyName).Replace("-", string.Empty).ToString();
+        
+        var property = typeof(T).GetProperty(purePropertyName,
+            BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        
+        if (property is null)
+            throw new ArgumentNullException(nameof(purePropertyName));
+        
+        var param = Expression.Parameter(typeof(T), purePropertyName);
+        var body = Expression.PropertyOrField(param, purePropertyName);
+        var selector = Expression.Lambda(body, param);
+        var methodName = descending ? "OrderByDescending" : "OrderBy";
+        var resultExpression = Expression.Call(
+            typeof(Queryable),
+            methodName,
+            [typeof(T), body.Type],
+            query.Expression,
+            Expression.Quote(selector));
+        
+        return query.Provider.CreateQuery<T>(resultExpression);
+    }
+    
+    public static IQueryable<TokenEntity> Filter(this IQueryable<TokenEntity> tokens, TokenFilter filter)
+    {
+        var query = tokens.AsQueryable();
+        
+        if (!string.IsNullOrEmpty(filter.Name))
+            query = query.Where(x => x.Name == filter.Name);
+        
+        if (!string.IsNullOrEmpty(filter.Symbol))
+            query = query.Where(x => x.Symbol == filter.Symbol);
+        
+        if (filter.Rank is not null)
+            query = query.Where(x => x.Rank == filter.Rank);
+        
+        if (filter.MinRank is not null)
+            query = query.Where(x => x.Rank >= filter.MinRank);
+        
+        if (filter.MaxRank is not null)
+            query = query.Where(x => x.Rank >= filter.MaxRank);
+        
+        if (filter.MinPrice is not null)
+            query = query.Where(x => x.Price >= filter.MinPrice);
+        
+        if (filter.MaxPrice is not null)
+            query = query.Where(x => x.Price >= filter.MaxPrice);
+        
+        if (filter.MaxMarketCap is not null)
+            query = query.Where(x => x.MarketCap >= filter.MaxMarketCap);
+        
+        if (filter.MinMarketCap is not null)
+            query = query.Where(x => x.MarketCap >= filter.MinMarketCap);
+        
+        if (filter.MinVolume is not null)
+            query = query.Where(x => x.Volume24H >= filter.MinVolume);
+        
+        if (filter.MaxVolume is not null)
+            query = query.Where(x => x.Volume24H >= filter.MaxVolume);
+        
+        if (filter.MinPercentChange is not null)
+            query = query.Where(x => x.PercentChange24H >= filter.MinPercentChange);
+        
+        if (filter.MaxPercentChange is not null)
+            query = query.Where(x => x.PercentChange24H >= filter.MaxPercentChange);
+        
+        return query;
+    }
+}
---
+++ src/Parser/Filtering/TokenFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace Parser.Filtering;
+
+public sealed class TokenFilter
+{
+    public string? Name { get; set; }
+    public string? Symbol { get; set; }
+    public int? Rank { get; set; }
+    public int? MinRank { get; set; }
+    public int? MaxRank { get; set; }
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+    public decimal? MaxMarketCap { get; set; }
+    public decimal? MinMarketCap { get; set; }
+    public long? MinVolume { get; set; }
+    public long? MaxVolume { get; set; }
+    public float? MinPercentChange { get; set; }
+    public float? MaxPercentChange { get; set; }
+}
