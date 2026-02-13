d0a63966acbfeb14c22a1915a22071f1476cb22f
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-13 13:23:27 +0100

    Re-migrate DB

---
+++ src/Parser/Migrations/20260213122316_AddRank.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parser.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "Tokens",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "Tokens");
+        }
+    }
+}
