b70be3897f7002fd7f57470421612b48c650d64c
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-13 19:49:52 +0100

    Add support for ordering, filtering and paginating of token on retrieve

--- src/Parser/Tokens/ITokenManager.cs
+++ src/Parser/Tokens/ITokenManager.cs
@@ -1,11 +1,18 @@
 ﻿using Parser.Data.Entities;
+using Parser.Filtering;
 using Parser.Results;
 
 namespace Parser.Tokens;
 
 public interface ITokenManager
 {
-    public ValueTask<List<TokenEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    public ValueTask<PageableResult<TokenEntity>> GetByQueryAsync(
+        TokenFilter filter,
+        int offset,
+        int limit,
+        string orderBy,
+        CancellationToken cancellationToken = default);
+    
     public ValueTask<Result> CreateAsync(IEnumerable<TokenEntity> tokens, 
         CancellationToken cancellationToken = default);
 }
--- src/Parser/Tokens/TokenManager.cs
+++ src/Parser/Tokens/TokenManager.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Parser.Data;
 using Parser.Data.Entities;
+using Parser.Extensions;
+using Parser.Filtering;
 using Parser.Results;
 
 namespace Parser.Tokens;
@@ -9,12 +11,31 @@ public sealed class TokenManager(AppDbContext context) : ITokenManager
 {
     private readonly AppDbContext _context = context;
 
-    public async ValueTask<List<TokenEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    public async ValueTask<PageableResult<TokenEntity>> GetByQueryAsync(
+        TokenFilter filter,
+        int offset, 
+        int limit, 
+        string orderBy,
+        CancellationToken cancellationToken = default)
     {
-        return await _context.Tokens.ToListAsync(cancellationToken);
+        var totalItems = await _context.Tokens.CountAsync(cancellationToken);
+        var items = await _context.Tokens.AsQueryable()
+            .Filter(filter)
+            .ApplyOrdering(orderBy)
+            .Skip(offset)
+            .Take(limit)
+            .ToListAsync(cancellationToken);
+        
+        return new PageableResult<TokenEntity>()
+        {
+            Offset =  offset,
+            Limit = limit,
+            Items = items,
+            TotalItems = totalItems
+        };
     }
 
-    public async ValueTask<Result> CreateAsync(IEnumerable<TokenEntity> tokens, 
+    public async ValueTask<Result> CreateAsync(IEnumerable<TokenEntity> tokens,
         CancellationToken cancellationToken = default)
     {
         await _context.Tokens.AddRangeAsync(tokens, cancellationToken);
