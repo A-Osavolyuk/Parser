7f13cebe77d11f31d8141730b800dacc1e9deae9
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-13 12:22:36 +0100

    Add initial migration

---
+++ src/Parser/Migrations/20260213112158_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parser.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Symbol = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    MarketCap = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PercentChange24H = table.Column<float>(type: "REAL", nullable: false),
+                    Volume24H = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Token_Name",
+                table: "Tokens",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tokens");
+        }
+    }
+}
